538d4a840cabe2754c4b7ff2176752df50f55ff4
Karin Touchie <ktouchie@gmail.com>
2015-12-21 12:52:45 +0100

    Create file, declare array

---
+++ select.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class select {
+
+	public static void main(String[] args) {
+        
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
+        
+        
+        
+        System.out.println(Arrays.toString(tab));
+    }
+}
