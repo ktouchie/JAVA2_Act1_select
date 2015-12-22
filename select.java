630e371895a5c4f3de27ea91289db12b3775cfa8
Karin Touchie <ktouchie@gmail.com>
2015-12-22 11:30:09 +0100

    Make var min local variable (reset min to i after each iteration

--- select.java
+++ select.java
@@ -3,23 +3,24 @@ class select {
 
 	public static void main(String[] args) {
         
-        int[] tab = {9,1,6,7,3,5,2,4,0,8};
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
         
         int i,j;
         
-        int min = 0;
-        
         for (i=0; i<tab.length-1; i++) {
+            
+            int min = i;
             
             for (j=i+1; j<tab.length; j++) {    // searches tab[1-9] and saves smaller nums to var min until the absolute min is found.
                 if (tab[j]<tab[min]) {
                 min = j;
                 }
             }
-            if (min != i && tab[min] < tab[i]) {                     // swaps i for min, so absolute min is now at tab[0]. Then repeats entire process to find new minimum & swap w/ tab[1].
+            if (min != i) {                     // swaps i for min, so absolute min is now at tab[0]. Then repeats entire process to find new minimum & swap w/ tab[1].
                 int x = tab[min];
                 tab[min] = tab[i];
                 tab[i] = x;
+                System.out.println(Arrays.toString(tab));
             }
         }
         System.out.println(Arrays.toString(tab));
